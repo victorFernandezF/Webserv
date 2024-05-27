74e5e70d946ebdaed6bb2e8e9192ce1a52d993b2
franlz.local <fran@lz.local>
2024-05-27 16:08:44 +0200

    Set default config file if not added by program arguments

--- src/main.cpp
+++ src/main.cpp
@@ -20,13 +20,18 @@ int	main(int argc, char **argv)
 
    sigaction(SIGINT, &sigIntHandler, NULL);*/
 
+	std::string file;
 
-	if (argc != 2)
+	if (argc > 2)
 		return (0);
+	if (argc == 1)
+		file = "./inc/default.conf";
+	else
+		file = argv[1];
 	try
 	{
 		Cluster cluster;
-		Config test(argv[1], cluster);
+		Config test(file, cluster);
 		std::cout << cluster << std::endl;
 		cluster.runServers();
 		cluster._closeFDs();
